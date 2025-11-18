850079c5f0d16add8ce79a4e2da42db542ddab05
GabrielBolduc <bolducGabriel@outlook.com>
2025-11-18 10:29:26 -0500

    commit debut visuel de du screen settings

--- Code/App.js
+++ Code/App.js
@@ -1,26 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-import feed_screen from './feed_screen';
-import movieList_screen from './movieList_screen'
-import myRatings_screen from './myRatings_screen'
-import rateMovie_screen from './rateMovie_screen'
-import settings_screen from './settings_screen'
+import FeedScreen from './feed_screen';
+import MovieListScreen from './movieList_screen';
+import MyRatingsScreen from './myRatings_screen';
+import RateMovieScreen from './rateMovie_screen';
+import SettingsScreen from './settings_screen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SettingsScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Code/settings_screen.js
+++ Code/settings_screen.js
@@ -1,19 +1,61 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Switch } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function settings(){
+
+    const [isDarkMode, setIsDarkMode] = useState(false)
+    const toggleSwitch = () =>setIsDarkMode(previousState => !previousState)
+    
     return(
         <View style={styles.container}>
-            <Text>Settings screen</Text>
+
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Account settings</Text>
+            </View>
+
+            <View style={styles.profileSection}>
+                <View style={styles.avatarContainer}>
+                    <Ionicons name="person-outline" size={24} color="black" />
+                </View>
+            </View>
+            <TouchableOpacity>
+                <Text style={styles.editPhotoText}>Changer la photo</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container:{
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#F5F5F5', 
         alignItems: 'center',
+    },
+    header: {
+        marginTop: 20,
+        marginBottom: 40,
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    profileSection: {
+        alignItems: 'center',
+        marginBottom: 50,
+    },
+    avatarContainer: {
+        width: 120,
+        height: 120,
+        borderRadius: 60, 
+        borderWidth: 2,
+        borderColor: '#4A6572', 
         justifyContent: 'center',
-  },
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        marginBottom: 10,
+    },
 })
